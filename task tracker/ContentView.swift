1c34a23c5ffd56b6912df7e09603cd87af032399
shabd <workwithshabd@gmail.com>
2026-03-18 02:31:44 +0530

    hi

--- task tracker/ContentView.swift
+++ task tracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello,mate world!")
+            Text("Hello,world!")
                 .bold()
         }
         .padding()
